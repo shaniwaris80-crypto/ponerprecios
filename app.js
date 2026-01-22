e38a29890a7ef33ae656922757a5ca30fee5acbb
shaniwaris80-crypto <shaniwaris80@gmail.com>
2026-01-22 17:16:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+{
+  "title": "PRECIOS",
+  "dateISO": "2026-01-22",
+  "products": [
+    { "provider": "ESMO", "name": "TOMATE DANIELA", "qty": 35, "oldPrice": "1.89" },
+    { "provider": "MONTENEGRO", "name": "ZANAHORIA", "qty": 40, "oldPrice": "" },
+    { "provider": "SIN PROVEEDOR", "name": "PIMIENTO RARO", "qty": 5, "oldPrice": "" }
+  ]
+}
